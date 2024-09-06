be35e36a377c23c71b6792aefd3de302926f0763
Parvathamabhinav <onlyforfreefire51@gmail.com>
2024-09-06 19:13:18 +0530

    Book added succesfully

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app=express();
 app.use(express.json())
 require("dotenv").config();
 require("./conn/conn")
-const user=require("./routes/user")
-app.use("/api/v1",user)
+const User=require("./routes/user")
+const Books=require("./routes/book")
+app.use("/api/v1",User)
+app.use("/api/v1",Books)
+
 app.get("/",(req,res)=>{
     res.send("Hello from backend ")
 })
--- routes/book.js
+++ routes/book.js
@@ -2,4 +2,32 @@ const router = require("express").Router();
 const User=require("../models/user")
 const bcrypt=require("bcryptjs")
 const jwt=require("jsonwebtoken")
+const Book=require("../models/book")
 const {authenticateToken}=require("./userAuth")
+
+
+//add book --admin
+router.post("/add-book",authenticateToken,async(req,res)=>{
+    try{
+        const {id}=req.headers;
+        const user=await User.findById(id);
+        if(user.role!=="admin"){
+            return res.status(400).json({message:"You are not having access to perform admin work"});
+        }
+        const book= new Book({
+            url:req.body.url,
+            title:req.body.title,
+            author:req.body.author,
+            price:req.body.price,
+            desc:req.body.price,
+            language:req.body.language,
+        })
+        await book.save();
+        res.status(200).json({message:"Book added succesfully"});
+    }
+    catch(error){
+        res.status(500).json({message:"Internal server error"})
+    }
+})
+
+module.exports=router;
