adc6df73ad8b799b7d05f9d71313f5af2ff51f10
Parvathamabhinav <onlyforfreefire51@gmail.com>
2024-09-06 16:27:30 +0530

    user Auth file created

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User=require("../models/user")
-const bcrypt=require("bcrypt")
+const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
 //Sign up
 router.post("/sign-up",async(req,res)=>{
     try{
@@ -42,6 +43,41 @@ router.post("/sign-up",async(req,res)=>{
         res.status(500).json({message:"Internal server error"})
     }
 })
+
+//sign in
+router.post("/sign-in",async(req,res)=>{
+    try{
+        const {username,password}=req.body;
+        const existingUser= await User.findOne({username});
+        if(!existingUser){
+            res.status(400).json({message:"Invalid credentials"});
+        }
+        await bcrypt.compare(password,existingUser.password,(err,data)=>{
+            if(data){
+                const authClaims=[
+                    {
+                        name:existingUser.username
+                    },
+                    {
+                        role:existingUser.role
+                    },
+                ];
+                const token=jwt.sign({authClaims},"bookStore123",{expiresIn:"30d",});
+                res.status(200).json({
+                    id:existingUser.id,
+                    role:existingUser.role,
+                    token:token
+                });
+            }else{
+                res.status(400).json({message:"Invalid password"});
+            }
+        })
+    }catch(error){
+        res.status(500).json({message:"Internal server error"})
+    }
+})
+
+
 
 
 
